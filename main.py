cc1663c843ba0c05c3582462de83ae15732262e6
Ummar Ikram <ummarikram@gmail.com>
2022-09-08 04:06:05 +0500

    Added Director Signature

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import shutil
 import pandas as pd
 from PIL import ImageFont,ImageDraw,Image
 
-# Coordinates for Name positioning, depends on certificate structure
-x_coordinate = 140
-y_coordinate = 660
+# Coordinates for Student Name positioning, depends on certificate structure
+studentX_coordinate = 150
+studentY_coordinate = 660
+
+# Coordinates for Director Name positioning, depends on certificate structure
+directorX_coordinate = 150
+directorY_coordinate = 1120
 
 # GENERTING CERTIFICATES
 def certificate_gen(name_ls):
@@ -31,13 +35,16 @@ def certificate_gen(name_ls):
         template_conv = cv2.cvtColor(template,cv2.COLOR_BGR2RGB)
         arr_img= Image.fromarray(template_conv)
         var_draw=ImageDraw.Draw(arr_img)
-        pacifico=ImageFont.truetype("fonts/Pacifico.ttf",90)
+        student_pacifico=ImageFont.truetype("fonts/Pacifico.ttf",90)
+        director_pacifico=ImageFont.truetype("fonts/Pacifico.ttf",50)
         
         # sofia_regular = ImageFont.truetype("fonts/Sofia-Regular.otf", 20) # CAN IMPLEMENT MORE THAN ONE FONT
        
-        var_draw.text((x_coordinate,y_coordinate),name,font=pacifico,fill='grey') # DEFINING CO-ORDS NAME AND FONT-COLOR
+        var_draw.text((studentX_coordinate,studentY_coordinate),name,font=student_pacifico,fill='grey') # DEFINING CO-ORDS NAME AND FONT-COLOR
+        var_draw.text((directorX_coordinate,directorY_coordinate), "Dr. Hammad Naveed",font=director_pacifico,fill='grey')
+
         final_res=cv2.cvtColor(np.array(arr_img),cv2.COLOR_RGB2BGR) # RE-CONVERTING IMAGE FROM ARRAY INFO
-        cv2.imwrite(os.path.join(folderName, "{}.jpg".format(name)), final_res) # TO SAVE THE FINAL OUTPUT
+        cv2.imwrite(os.path.join(folderName, "{}.png".format(name)), final_res) # TO SAVE THE FINAL OUTPUT
         print("{}'s certificate generated".format(name))
 
       
